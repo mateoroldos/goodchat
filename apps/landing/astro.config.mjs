3f82a0ffe8f454e54166df7e1e5e76e1ad6426fd
Mateo Roldos <mateoroldos19@gmail.com>
2026-03-23 00:06:00 -0300

    Add landing page

---
+++ apps/landing/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  integrations: [svelte()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
